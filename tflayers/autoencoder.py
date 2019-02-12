a3ec85976f966fb7392edd56c2df1442d284e218
Sergey <xxsmotur@gmail.com>
2019-02-12 23:13:21 +0500

    Add variational autoencoder model

--- tflayers/autoencoder.py
+++ tflayers/autoencoder.py
@@ -31,3 +31,52 @@ class Autoencoder:
                                                  self.weights[self.weights_names[i]]),
                                        self.biases[self.biases_names[i]]))
         return result
+
+class VariationalAutoencoder:
+
+    def __init__(self, n_input, n_hidden, weights, optimizer = tf.train.AdamOptimizer()):
+        self.n_input = n_input
+        self.n_hidden = n_hidden
+        self.weights = weights
+
+        self.x = tf.placeholder(tf.float32, [None, self.n_input])
+        self.z_mean = tf.add(tf.matmul(self.x, self.weights['w1']), self.weights['b1'])
+        self.z_log_sigma_sq = tf.add(tf.matmul(self.x, self.weights['log_sigma_w1']), self.weights['log_sigma_b1'])
+        eps = tf.random_normal(tf.stack([tf.shape(self.x)[0], self.n_hidden]), 0, 1, dtype = tf.float32)
+        self.z = tf.add(self.z_mean, tf.multiply(tf.sqrt(tf.exp(self.z_log_sigma_sq)), eps))
+        self.reconstruction = tf.add(tf.matmul(self.z, self.weights['w2']), self.weights['b2'])
+        reconstr_loss = 0.5 * tf.reduce_sum(tf.pow(tf.subtract(self.reconstruction, self.x), 2.0))
+        latent_loss = -0.5 * tf.reduce_sum(1 + self.z_log_sigma_sq
+                                           - tf.square(self.z_mean)
+                                           - tf.exp(self.z_log_sigma_sq), 1)
+        self.cost = tf.reduce_mean(reconstr_loss + latent_loss)
+        self.optimizer = optimizer.minimize(self.cost)
+
+        init = tf.global_variables_initializer()
+        self.sess = tf.Session()
+        self.sess.run(init)
+
+    def partial_fit(self, X):
+        cost, opt = self.sess.run((self.cost, self.optimizer), feed_dict={self.x: X})
+        return cost
+
+    def calc_total_cost(self, X):
+        return self.sess.run(self.cost, feed_dict = {self.x: X})
+
+    def transform(self, X):
+        return self.sess.run(self.z_mean, feed_dict={self.x: X})
+
+    def generate(self, hidden = None):
+        if hidden is None:
+            hidden = self.sess.run(tf.random_normal([1, self.n_hidden]))
+        return self.sess.run(self.reconstruction, feed_dict={self.z: hidden})
+
+    def reconstruct(self, X):
+        return self.sess.run(self.reconstruction, feed_dict={self.x: X})
+
+    def getWeights(self):
+        return self.sess.run(self.weights['w1'])
+
+    def getBiases(self):
+        return self.sess.run(self.weights['b1'])
+
